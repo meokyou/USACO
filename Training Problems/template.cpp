dd4aa313588018ee9c09463aab22de451b5934b8
Christian Anderson <70417374+meokyou@users.noreply.github.com>
2022-05-04 01:31:31 -0500

    Add sol. template

---
+++ Training Problems/template.cpp
@@ -0,0 +1,28 @@
+/*
+ID: christi104
+TASK: task
+LANG: C++
+*/
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ld long double
+#define arr array
+#define v  vector
+#define pb push_back
+#define gl getline
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ifstream fin ("task.in");
+    ofstream fout ("task.out");
+    
+    // Start here
+    
+    return 0;
+}
